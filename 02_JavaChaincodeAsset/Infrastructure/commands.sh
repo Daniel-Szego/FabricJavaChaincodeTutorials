e43c88e386725b04606a500a3fb3b099e0c36db1
dsz <szego.daniel@gmail.com>
2020-01-03 22:22:45 +0100

    infrastructure commands

--- 02_JavaChaincodeAsset/Infrastructure/commands.sh
+++ 02_JavaChaincodeAsset/Infrastructure/commands.sh
@@ -3,7 +3,7 @@
 # Copyright Interticket
 #
 # INSTALL:
-# 1. copy the gradle files and the scr files apart from test to the chaincode/01_HelloJavaChaincode directory
+# 1. copy the gradle files and the scr files apart from test to the chaincode/02_JavaChaincodeAsset directory
 # 2. run on the cli container, with either
 #    docker exec cli scripts/balancetrackerinit.sh
 # 3. or login to the cli container and execute the commands manually
@@ -15,7 +15,7 @@ echo "##### installing chaincode #########"
 echo "#####################################################"
 echo
 
-peer chaincode install -l java -n mycc -v v1 -p /opt/gopath/src/github.com/01_HelloJavaChaincode
+peer chaincode install -l java -n mycc -v v1 -p /opt/gopath/src/github.com/02_JavaChaincodeAsset
 
 sleep 25
 
@@ -25,33 +25,46 @@ echo "##### initializing chaincode #########"
 echo "#####################################################"
 echo
 
-peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc  -v v1 -c '{"Args":["init","HelloWorld"]}' -P 'OR ("Org1MSP.member")'
+peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc  -v v1 -c '{"Args":[]}' -P 'OR ("Org1MSP.member")'
 
 sleep 120
 
 echo
 echo "#####################################################"
-echo "##### test getHelloWorld #########"
+echo "##### test setHouse #########"
+echo "#####################################################"
+echo
+echo "Test setHouse"
+echo
+
+peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["setHouse","houseID123","2","testCountry","testCity","testStreet","4"]}'
+
+echo
+echo "#####################################################"
+echo "##### test getHouse #########"
 echo "#####################################################"
 echo
 
-peer chaincode query -C mychannel -n mycc -c '{"Args":["getHelloWorld"]}'
+peer chaincode query -C mychannel -n mycc -c '{"Args":["getHouse","houseID123"]}'
 
 sleep 5
 
+echo
+echo "#####################################################"
+echo "##### test buildNewRoom #########"
+echo "#####################################################"
+echo
+echo "Test buildNewRoom"
+echo
+
+peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["buildNewRoom","houseID123","4"]}'
 
 echo
 echo "#####################################################"
-echo "##### test setHelloWorldMessage #########"
+echo "##### test getHouse again #########"
 echo "#####################################################"
 echo
-echo "Test setHelloWorldMessag"
-echo
 
-peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["setHelloWorldMessage", "HalloWelt"]}'
-
-echo "Test getHelloWorld"
-
-peer chaincode query -C mychannel -n mycc -c '{"Args":["getHelloWorld"]}'
+peer chaincode query -C mychannel -n mycc -c '{"Args":["getHouse","houseID123"]}'
 
 
--- README.md
+++ README.md
@@ -18,6 +18,24 @@ Chaincode examples for Java
 
 2. JavaChaincodeAsset: admiistrating a house on the blockchain
 
+- copy the gradle files and the scr files apart from test to the chaincode/02_JavaChaincodeAsset directory
+- start a Hyperledger Fabric network (like fabric-samples/basic-network): pay attention if the cli drive mappings are valid
+- run on the cli container
+   docker exec -it cli /bin/bash
+- install chaincode
+   peer chaincode install -l java -n mycc -v v1 -p /opt/gopath/src/github.com/02_JavaChaincodeAsset
+- initiate chaincode
+   peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc  -v v1 -c '{"Args":[]}' -P 'OR ("Org1MSP.member")'
+- create new house
+   peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["setHouse","houseID123","2","testCountry","testCity","testStreet","4"]}'
+- read the house
+   peer chaincode query -C mychannel -n mycc -c '{"Args":["getHouse","houseID123"]}'
+- update the number of rooms for the house
+   peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["buildNewRoom","houseID123","4"]}'
+- read the modfied house
+   peer chaincode query -C mychannel -n mycc -c '{"Args":["getHouse","houseID123"]}'
+
+
 
 
 
